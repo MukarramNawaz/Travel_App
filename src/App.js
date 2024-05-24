1929f2bed0549d164b6534b3e33d93b5bdaf67a2
Mukarram Nawaz <156927769+MukarramNawaz@users.noreply.github.com>
2024-05-24 20:00:28 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import MainImage from './components/MainImage';
+import MidPlaces from './components/MidPlaces';
+import Features from './components/Features';
+import Footer from './components/Footer';
+import './css/style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <MainImage />
+      <MidPlaces />
+      <Features />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
